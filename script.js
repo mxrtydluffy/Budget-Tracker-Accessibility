f48dc6f9724a4cbce9bea68c34e5ca307d6ebb6a
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-05 14:56:31 -0800

    Added fixes & determined JS ID

---
+++ script.js
@@ -0,0 +1,13 @@
+class UI {
+    constructor() {
+        constructor() {
+            this.budgetFeedback = document.querySelector(".budget-feedback");
+            this.expenseFeedback = document.querySelector(".expebse-feedback");
+            this.budgetForm = document.getElementById("budget-input");
+            this.budgetInput = document.getElementById("budget-amount");
+            this.expenseAmount = document.getElementById("expense-amount");
+            this.balance = document.getElementById("balance");
+            this.balanceAmount = document.getElementById("balance-amount");
+        }
+    }
+}
