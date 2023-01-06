3d95152ce7f2f965b23e8a36e9c1772557e391dc
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-05 22:54:38 -0800

    buggy not displaying console

--- script.js
+++ script.js
@@ -9,27 +9,30 @@ class UI {
     constructor() {
         this.budgetFeedback = document.querySelector(".budget-feedback");
         this.expenseFeedback = document.querySelector(".expense-feedback");
-        this.budgetForm = document.getElementById("budget-input");
-        this.budgetInput = document.getElementById("budget-amount");
+        this.budgetForm = document.getElementById("budget-form")
+        this.budgetInput = document.getElementById("budget-input");
+        this.budgetAmount = document.getElementById("budget-amount");
         this.expenseAmount = document.getElementById("expense-amount");
         this.balance = document.getElementById("balance");
         this.balanceAmount = document.getElementById("balance-amount");
         this.expenseForm = document.getElementById("expense-form");
         this.expenseInput = document.getElementById("expense-input");
         this.amountInput = document.getElementById("amount-input");
-        this.expenseList = document.getAnimations("expense-list");
+        this.expenseList = document.getElementById("expense-list");
         this.itemList = [];
         this.itemID = 0;
     }
 
     // Submit budget method
     submitBudgetForm(){
-        console.log('Successfully submitted');
         const value = this.budgetInput.value;
         // Declaring the values
-        if(value === '' || value <0){
+        if(value === '' || value < 0){
             this.budgetFeedback.classList.add('showItem');  // ClassList has add or remove
             this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;  // Inner HTML will not show p tag, if were to put textcontent it will show. Displaying ticks allows us to accomplish this.
+            setTimeout(function(){
+                this.budgetFeedback.classList.remove('showItem');
+            },4000)
         }
     }
 }
@@ -43,19 +46,19 @@ function eventListenters() {
     const ui = new UI()
 
     // Budget form submit
-    budgetForm.addEventListener('submit', function(event){
+    budgetForm.addEventListener('submit', function(event) {
         // Not going to be resubmitting everytime | No need to refresh
         event.preventDefault();
         ui.submitBudgetForm();
     });
 
     // Expense form submit
-    expenseForm.addEventListener('submit', function(event){
+    expenseForm.addEventListener('submit', function(event) {
         event.preventDefault();
     });
 
     // Expense click
-    expenseList.addEventListener('click', function(){});
+    expenseList.addEventListener('click', function() {});
 }
 
 document.addEventListener('DOMContentLoaded', function() {
