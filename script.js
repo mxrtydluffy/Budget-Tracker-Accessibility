9a4c1aeeec36fee55dfa61c9945b136a254adee0
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-05 21:24:39 -0800

    Added ID & formatted code

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+// Set variable to new UI and getting the instance of the class
+
 class UI {
     constructor() {
-        constructor() {
-            this.budgetFeedback = document.querySelector(".budget-feedback");
-            this.expenseFeedback = document.querySelector(".expebse-feedback");
-            this.budgetForm = document.getElementById("budget-input");
-            this.budgetInput = document.getElementById("budget-amount");
-            this.expenseAmount = document.getElementById("expense-amount");
-            this.balance = document.getElementById("balance");
-            this.balanceAmount = document.getElementById("balance-amount");
-        }
+        this.budgetFeedback = document.querySelector(".budget-feedback");
+        this.expenseFeedback = document.querySelector(".expense-feedback");
+        this.budgetForm = document.getElementById("budget-input");
+        this.budgetInput = document.getElementById("budget-amount");
+        this.expenseAmount = document.getElementById("expense-amount");
+        this.balance = document.getElementById("balance");
+        this.balanceAmount = document.getElementById("balance-amount");
+        // Add more ElementID
     }
 }
+
+function eventListenters(){
+    const budgetForm = document.getElementById('budget-form');
+    const budgetForm = document.getElementById('budget-form');
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    eventListenters();
+})
