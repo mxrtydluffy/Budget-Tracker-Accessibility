163c9eccdba75c63dcfc314c459a0c2f1c3b67bb
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-05 23:18:08 -0800

    No debug errors but console detects a problem

--- script.js
+++ script.js
@@ -27,9 +27,11 @@ class UI {
     submitBudgetForm(){
         const value = this.budgetInput.value;
         // Declaring the values
-        if(value === '' || value < 0){
+        if(value==='' || value < 0) {
             this.budgetFeedback.classList.add('showItem');  // ClassList has add or remove
             this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;  // Inner HTML will not show p tag, if were to put textcontent it will show. Displaying ticks allows us to accomplish this.
+            const self = this;
+            console.log(this);
             setTimeout(function(){
                 this.budgetFeedback.classList.remove('showItem');
             },4000)
@@ -38,29 +40,29 @@ class UI {
 }
 
 function eventListenters() {
-    const budgetForm = document.getElementById('budget-form');
-    const expenseForm = document.getElementById('expense-form');
-    const expenseList = document.getElementById('expense-list');
+    const budgetForm = document.getElementById("budget-form");
+    const expenseForm = document.getElementById("expense-form");
+    const expenseList = document.getElementById("expense-list");
 
     // New instance of UI Class allows to get its methods & properties.
     const ui = new UI()
 
     // Budget form submit
-    budgetForm.addEventListener('submit', function(event) {
+    budgetForm.addEventListener("submit", function(event) {
         // Not going to be resubmitting everytime | No need to refresh
         event.preventDefault();
         ui.submitBudgetForm();
     });
 
     // Expense form submit
-    expenseForm.addEventListener('submit', function(event) {
+    expenseForm.addEventListener("submit", function(event) {
         event.preventDefault();
     });
 
     // Expense click
-    expenseList.addEventListener('click', function() {});
+    expenseList.addEventListener("click", function() {});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     eventListenters();
 });
