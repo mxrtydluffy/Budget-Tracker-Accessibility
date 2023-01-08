7a1d11321f025452b28f353d664b7370a54e0973
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-07 17:28:57 -0800

    WOO got it to work

--- script.js
+++ script.js
@@ -108,20 +108,19 @@ class UI {
         const div = document.createElement('div');
         div.classList.add('expense');
         div.innerHTML = `
-        <div class="expense-item d-flex justify-content-between align-items-baseline">
+        <div class="expense-item d-flex justify-content-around">
 
-         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
-         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+            <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+            <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
 
-         <div class="expense-icons list-item">
-
-          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
-           <i class="fas fa-edit"></i>
-          </a>
-          <a href="#" class="delete-icon" data-id="${expense.id}">
-           <i class="fas fa-trash"></i>
-          </a>
-         </div>
+            <div class="expense-icons list-item">
+                <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+                <i class="fas fa-edit"></i>
+                </a>
+                <a href="#" class="delete-icon" data-id="${expense.id}">
+                <i class="fas fa-trash"></i>
+                </a>
+            </div>
         </div>
         `;
         this.expenseList.appendChild(div);
@@ -146,12 +145,13 @@ function eventListenters() {
     budgetForm.addEventListener("submit", function(event) {
         // Not going to be resubmitting everytime | No need to refresh
         event.preventDefault();
-        ui.submitExpenseForm();
+        ui.submitBudgetForm();
     });
 
     // Expense form submit
     expenseForm.addEventListener("submit", function(event) {
         event.preventDefault();
+        ui.submitExpenseForm();
     });
 
     // Expense click
